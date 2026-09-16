5dc19940e53f72694e8ee045f043307b6a3deb9a
emanuelleabreuc <emanuelleslavey@gmail.com>
2026-09-16 17:22:07 -0300

    criação da posição da cobra na tela

--- snakeGame.py
+++ snakeGame.py
@@ -1,24 +1,37 @@
 from turtle import Screen, Turtle
+import time
 
 screen = Screen() 
 screen.setup(width=600, height=600)
 screen.bgcolor("green yellow")
 screen.title("Snake Game")
-screen.tracer(0)
+screen.tracer(0) #desativa a animacao da tela
 
-screen.exitonclick() 
+PASSO = 20
+LIMITE = 200
+
+
 
 segments = [] #lista vazia para armazenar os segmentos da cobra
 starting_positions = [(0, 0), (-20, 0), (-40, 0)] #posicoes em x e y para os segmentos da cobra (sao 3)
-for posicao in starting_positions:
-    novo_segmento = Turtle("square") 
-    novo_segmento.color("black")
-    novo_segmento.penup() #perguntar pra raissa
-    novo_segmento.shapesize(1,1)
-    novo_segmento.goto(posicao) #coloca a cobra na posicao inicial
-    segments.append(novo_segmento)
 
-screen.update()
+def criar_segmento(posicao):
+    novo_segmento = Turtle("square") #cria um segmento da cobra
+    novo_segmento.color("black") #cor do segmento
+    novo_segmento.penup() #desativa o desenho do segmento
+    novo_segmento.shapesize(1,1)
+    novo_segmento.goto(posicao) #posiciona o segmento na posicao especificada
+    segments.append(novo_segmento) #adiciona o segmento na lista de segmentos
+
+ ################# screen.update()
+
+for posicao in starting_positions:
+    criar_segmento(posicao) 
+
+
+################## screen.exitonclick()
+
+
 
 
 
