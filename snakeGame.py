7333e9983b60d4c6c0e5ab42accde0d8ba7b18aa
emanuelleabreuc <emanuelleslavey@gmail.com>
2026-09-24 23:10:26 -0300

    mudanças na movimentação

--- snakeGame.py
+++ snakeGame.py
@@ -66,18 +66,18 @@ screen.onkey(ir_para_direita, "Right")
 
 
 def mover():
-    novo_x = segments[0].xcor() + aim_x #segments[0] é a CABEÇA da cobra (o primeiro segmento da lista)
-    #.xcor() pega a posição X atual dela na tela
-    #somamos aim_x (do passo 1) pra calcular pra onde ela deve ir no eixo X
-    #ex: se está em x=100 e aim_x=20, novo_x vira 120 (andou 20px pra direita)
-    novo_y = segments[0].ycor() + aim_y 
-    segments[0].goto(novo_x, novo_y)   #move de fato a cabeça para a nova posição calculada
+    posicoes = [segmento.pos() for segmento in segments]
+ 
+    for i in range(len(segments) - 1, 0, -1):
+        segments[i].goto(posicoes[i - 1])
+ 
+    novo_x = segments[0].xcor() + aim_x
+    novo_y = segments[0].ycor() + aim_y
+    segments[0].goto(novo_x, novo_y)
+ 
     screen.update()
-    screen.ontimer(mover, 100) #isso agenda: "daqui a 100 milissegundos (0.1 segundo), chame a função
+    screen.ontimer(mover, 100)
+ 
 mover()
-screen.exitonclick() #mantém a janela aberta até você clicar nela pra fechar
-
-
-
-
-
+ 
+screen.exitonclick()
